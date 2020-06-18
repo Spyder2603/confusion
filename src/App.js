7146e926ecdb04c8d47c22899ae7736fa53fc690
Spyder2603 <123saurabh2603@gmail.com>
2020-06-19 03:29:19 +0530

    components part 1

--- src/App.js
+++ src/App.js
@@ -4,20 +4,19 @@ import {
   Navbar,
   NavbarBrand
 } from 'reactstrap';
+import Menu from './components/MenuComponents';
 import './App.css';
 
 function App() {
-  return ( <
-    div className = "App" >
-    <
-    Navbar dark color = "primary" >
-    <
-    div className = "container" >
-    <
-    NavbarBrand href = "/" > Ristorante Con Fusion < /NavbarBrand> <
-    /div> <
-    /Navbar> <
-    /div>
+  return (
+    <div className="App">
+      <Navbar dark color = "primary">
+        <div className = "container">
+          <NavbarBrand href = "/" > Ristorante Con Fusion </NavbarBrand> 
+        </div>
+      </Navbar>
+      <Menu />
+    </div>
   );
 }
 
